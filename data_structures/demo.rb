f45e4448126fb65c502a3fef0eefbf6d42f197e8
jeffthemaximum <frey.maxim@gmail.com>
2016-07-18 16:39:34 -0600

    intro to bst

---
+++ data_structures/demo.rb
@@ -0,0 +1,39 @@
+module DataStructures
+  
+  BSTNode = Struct.new(:value, :left_node, :right_node)
+  
+  class BinarySearchTree
+    attr_accessor :root
+
+    def initialize(root = nil)
+      @root = BSTNode.new(root, nil, nil) unless root.nil?
+    end
+
+    def insert(value)
+      node = BSTNode.new(value, nil, nil)
+      @root = node and return if @root.nil?
+      insert_at_node(@root, node)
+    end
+
+    def insert_at_node(tree, node)
+      # if tree node is nil, make it new node
+      tree = node and return if tree.nil?
+
+      # if tree node and new node have same value, return tree
+      return tree if tree.value == node.value
+
+      # if node val is less than tree val
+      if node.value < tree.value
+        # if there's a value to left of tree, move down and try to insert again
+        return insert_at_node(tree.left, node) unless tree.nil?
+        # insert node to left of tree node
+        return tree.left_node = node
+      end
+
+      # same things as above, but to the right, if the node.val > tree.val
+      return insert_at_node(tree.right, node) unless tree.nil?
+      return tree.right_node = node
+    end
+  end
+
+end
--- data_structures/readme.md
+++ data_structures/readme.md
@@ -1,1 +1,11 @@
 # data structures
+
+### BST
+
+- From here: https://gist.github.com/jonaphin/2645532
+
+- This codez uses `Struct`s, which are written about here: https://www.leighhalliday.com/ruby-struct
+
+- `Struct`'s are basically simple classes without an `initialize` method or any other methods.
+
+- see `demo.rb` for an example of a BST.
